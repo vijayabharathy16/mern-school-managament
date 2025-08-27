f691ead7b4893f198e06a7f0b0c1aed457474c50
vijayabharathy16 <vijayabharathy753@gmail.com>
2025-08-27 19:11:17 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-// @type {import('tailwindcss').Config}
 export default defineConfig({
   content: [
     "./index.html",
@@ -19,6 +18,6 @@ export default defineConfig({
     }
   },
   plugins: [react(), tailwindcss()],
-
+ base: '/',
   
 })
